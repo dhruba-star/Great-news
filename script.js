c4fef47f1aba0715a503a5b8583d7204c5723709
dhruba-star <indiracgps@gmail.com>
2025-04-25 18:39:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ toggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark');
   toggleBtn.textContent = document.body.classList.contains('dark') ? 'Light Mode' : 'Dark Mode';
 });
+
+/*--scrollbar features --/*
+  let lastScroll = 0;
+  const header = document.querySelector("header");
+
+  window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll > lastScroll && currentScroll > 100) {
+      // Scrolling down
+      header.style.top = "-100px"; // Hide header
+    } else {
+      // Scrolling up
+      header.style.top = "0";
+    }
+
+    lastScroll = currentScroll;
+  });
